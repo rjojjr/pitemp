d81a81e184e98311307435ddf7aa24e08bb2dadd
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2019-08-30 07:55:16 -0500

    Bugfixes and additions

--- src/main/java/ks/pitemp/Main.java
+++ src/main/java/ks/pitemp/Main.java
@@ -4,10 +4,28 @@ import py4j.GatewayServer;
 
 public class Main {
 
+    private Update app;
+
+    public Main(){
+        app = new Update();
+    }
+
     public static void main(String[] args) throws  Exception{
-        Update app = new Update();
+
         // app is now the gateway.entry_point
-        GatewayServer server = new GatewayServer(app);
+        System.out.println("Starting gateway server");
+        GatewayServer server = new GatewayServer(new Main());
         server.start();
+    }
+
+    public Update getApp() throws Exception{
+        System.out.println("Python connected to server");
+        System.out.println("Logging on...");
+        if(app.logon()){
+            System.out.println("Logged on");
+        }else{
+            System.out.println("Failed to logon...");
+        }
+        return app;
     }
 }
--- src/main/java/ks/pitemp/Update.java
+++ src/main/java/ks/pitemp/Update.java
@@ -1,34 +1,58 @@
 package ks.pitemp;
 
+import kirchnersolutions.javabyte.driver.common.driver.DatabaseResults;
 import kirchnersolutions.javabyte.driver.common.driver.Transaction;
 import kirchnersolutions.javabyte.driver.singleclient.SingleClient;
 
+import javax.swing.text.SimpleAttributeSet;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Update {
 
     private String temp = "", humidity = "";
-    private SingleClient client = new SingleClient("192.168.1.25", "", 4444, "pizero1", "Password44#");
+    private SingleClient client = new SingleClient("192.168.1.25", " ", 4444, "pizero1", "Password44#");
 
     public void update(String temp, String humidity) throws Exception{
-        updateDB(temp, humidity);
+        String date = CalenderConverter.getMonthDayYearHourMinuteSecond(System.currentTimeMillis(), "~", "~");
+        System.out.println("Update Received: Temp: " + temp + " Humidity: " + humidity + " Date: " + date);
+        updateDB(temp, humidity, date);
     }
 
-    private void updateDB(String temp, String humidity) throws Exception{
+    public boolean logon() throws Exception{
+        return client.logon();
+    }
+
+    private void updateDB(String temp, String humidity, String date) throws Exception{
         this.humidity = humidity;
         this.temp = temp;
-        client.logon();
-        Transaction transaction = new Transaction();
-        transaction.setRequestTime(System.currentTimeMillis());
-        transaction.setUsername("pizero1");
-        Map<String, String> row = new HashMap<>();
-        row.put("user", "pizero1");
-        row.put("temp", temp);
-        row.put("humidity", humidity);
-        row.put("date", CalenderConverter.getMonthDayYearHourMinuteSecond(System.currentTimeMillis(), ":", "~"));
-        transaction.setPut(row);
-        client.sendCommand(transaction);
+        if(!logon()){
+            System.out.println("Update failed...");
+            System.out.println("Failed to logon...");
+        }else{
+            Transaction transaction = new Transaction();
+            transaction.setRequestTime(System.currentTimeMillis());
+            //transaction.setUsername("pizero1");
+            List<Map<String, String>> rows = new ArrayList<>();
+            Map<String, String> row = new HashMap<>();
+            row.put("username", "office");
+            row.put("temp", temp);
+            row.put("humidity", humidity);
+            row.put("date", date);
+            rows.add(row);
+            transaction.setNewRows(rows);
+            transaction.setOperation("CREATE ROWS ADVANCED PiTemps");
+            System.out.println("Sending update...");
+            DatabaseResults results = client.sendCommand(transaction);
+            if(results.isSuccess()){
+                System.out.println("Update Successful");
+            }else {
+                System.out.println("Update Unsuccessful");
+            }
+        }
+
     }
 
     public String getHumidity() {
